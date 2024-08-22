a0b8cc42527a57e8940d5a80f4f5d2d16c967e73
Alessandro Pino <alessandropino@caern.com.br>
2024-08-22 17:43:03 -0300

    Add uso DadosEpisodio e DadosTemporada ao App

--- src/main/java/br/com/caern/cathub/CathubApplication.java
+++ src/main/java/br/com/caern/cathub/CathubApplication.java
@@ -1,12 +1,17 @@
 package br.com.caern.cathub;
 
+import br.com.caern.cathub.model.DadosEpisodio;
 import br.com.caern.cathub.model.DadosSerie;
+import br.com.caern.cathub.model.DadosTemporada;
 import br.com.caern.cathub.service.ConsumoApi;
 import br.com.caern.cathub.service.ConverteDados;
 import br.com.caern.cathub.service.DadosEnv;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class CathubApplication implements CommandLineRunner {
@@ -26,10 +31,20 @@ public class CathubApplication implements CommandLineRunner {
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
 
+		json = consumoApi.obterDados("https://www.omdbapi.com/?t=game+of+thrones&season=1&episode=1&apikey=" + apiKey);
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+		for(int i = 1; i <= dados.totalTemporadas(); i++) {
+			json = consumoApi.obterDados("https://www.omdbapi.com/?t=game+of+thrones&season=" + i + "&apikey=" + apiKey);
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+
+		temporadas.forEach(System.out::println);
+
 		//json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
 		//System.out.println(json);
-
-
-
 	}
 }
